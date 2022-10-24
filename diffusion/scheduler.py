bbf4050da2e0b6c4a62f5a2a9e1dc4209220ccae
Otto Martikainen <martikainenotto@gmail.com>
2022-10-24 19:21:15 +0300

    Fix scheduler splitting

--- diffusion/scheduler.py
+++ diffusion/scheduler.py
@@ -3,6 +3,8 @@ from diffusion.db import save_task
 def schedule_task(task_options, target_size=3):
     '''Split long tasks into size of target_size'''
     if task_options.loop_to_loop:
+        # if task wants to reuse image from loop to loop
+        # it cannot be splitted to multiple tasks (yet)
         save_task(task_options)
         return 1
     iterations = task_options.n_samples
@@ -10,8 +12,12 @@ def schedule_task(task_options, target_size=3):
     steps_in_task = iterations * steps_in_program
     tasks_scheduled = 0
     if steps_in_task > target_size:
-        iterations_per_task = max(1, min(1, int(target_size / steps_in_program)))
-        steps_per_task = iterations_per_task * steps_in_program
+        if steps_in_program >= target_size:
+            iterations_per_task = 1
+            steps_per_task = steps_in_program
+        else:
+            iterations_per_task = int(target_size/steps_in_program)
+            steps_per_task = steps_in_program * iterations_per_task
         tasks = int(steps_in_task / steps_per_task)
         task_options.n_samples = iterations_per_task
         task_group_id = None
@@ -20,11 +26,13 @@ def schedule_task(task_options, target_size=3):
             tasks_scheduled += 1
             if task_group_id is None:
                 task_group_id = task['task_group_id']
-        # one short, add one
-        if tasks * steps_per_task < steps_in_task:
-            task_options.n_samples = 1 
+        # Task should still do more steps, add one loop
+        steps_completed = tasks * steps_per_task
+        if steps_completed < steps_in_task:
+            task_options.n_samples = max(1, int((steps_in_task - steps_completed) / steps_in_program))
             save_task(task_options, task_group_id)
             tasks_scheduled += 1
         return tasks_scheduled
+    # Task smaller than target, no need to split
     save_task(task_options)
     return 1
